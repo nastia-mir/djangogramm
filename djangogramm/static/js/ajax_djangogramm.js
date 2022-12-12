d82d863eff859d58182ac93f4c5c366b2820a6be
Anastasiia Myronchenko <nastasia.ua@gmail.com>
2022-12-12 14:59:10 +0200

    task 14

--- djangogramm/static/js/ajax_djangogramm.js
+++ djangogramm/static/js/ajax_djangogramm.js
@@ -4,7 +4,9 @@ $(document).ready( function() {
         e.preventDefault()
         const post_id = $(this).attr("data-postId");
         const url = $(this).attr('action')
-        const likes = parseInt($(this).attr("data-likes"))
+
+        let res;
+        const likes_count = parseInt($(this).attr("data-likes"))
 
         $.ajax(
         {
@@ -16,16 +18,19 @@ $(document).ready( function() {
             },
             success: function( data )
             {
-                 $('.likeForm').hide();
-                 $('.unlikeForm').show()
+                 $('.likeForm').addClass("d-none");
+                 $('.unlikeForm').removeClass("d-none");
+                 likes = likes_count + 1
             }
          })
     });
 
-$('.unlikeForm').submit(function(e){
+    $('.unlikeForm').submit(function(e){
         e.preventDefault()
         const post_id = $(this).attr("data-postId");
         const url = $(this).attr('action')
+
+        let res;
         const likes = parseInt($(this).attr("data-likes"))
 
         $.ajax(
@@ -38,8 +43,59 @@ $('.unlikeForm').submit(function(e){
             },
             success: function( data )
             {
-                 $('.unlikeForm').hide();
-                 $('.likeForm').show()
+                 $('.unlikeForm').addClass("d-none");
+                 $('.likeForm').removeClass("d-none");
+                 res = likes - 1
+            }
+         })
+    });
+
+    $('.followForm').submit(function(e){
+        e.preventDefault()
+        const user_id = $(this).attr("data-uid");
+        const url = $(this).attr('action')
+
+        let res;
+        const followers = parseInt($(this).attr("data-followers"))
+
+        $.ajax(
+        {
+            type:"POST",
+            url: url,
+            data:{
+                'csrfmiddlewaretoken': window.CSRF_TOKEN,
+                'user_id': user_id
+            },
+            success: function( data )
+            {
+                 $('.followForm').addClass("d-none");
+                 $('.unfollowForm').removeClass("d-none");
+                 res = followers - 1
+            }
+         })
+    });
+
+    $('.unfollowForm').submit(function(e){
+        e.preventDefault()
+        const user_id = $(this).attr("data-uid");
+        const url = $(this).attr('action')
+
+        let res;
+        const followers = parseInt($(this).attr("data-followers"))
+
+        $.ajax(
+        {
+            type:"POST",
+            url: url,
+            data:{
+                'csrfmiddlewaretoken': window.CSRF_TOKEN,
+                'user_id': user_id
+            },
+            success: function( data )
+            {
+                 $('.unfollowForm').addClass("d-none");
+                 $('.followForm').removeClass("d-none");
+                 res = followers - 1
             }
          })
     });
